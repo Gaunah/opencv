341667490c63afe893597dcf606a909fab47bd7c
gaunah <christoph@zelinski.de>
2015-08-04 14:24:51 +0200

    WIP - get absdiff img

--- motion_tracking.cpp
+++ motion_tracking.cpp
@@ -38,19 +38,24 @@ int main(){
 		return EXIT_FAILURE;
 	}
 
-	Mat imgOrginal;	
+	Mat imgOrginal, imgOrginal2, imgGray1, imgGray2, imgDiff;	
 	bool tracking = false, showCoords = false;
 	while(true){
 		if(!cap.read(imgOrginal)){
 			std::cerr << "could not read frame from video stream!" << std::endl;
 			return EXIT_FAILURE;
 		}
-		
+		cvtColor(imgOrginal, imgGray1, COLOR_BGR2GRAY);
+		cap.read(imgOrginal2);
+		cvtColor(imgOrginal2, imgGray2, COLOR_BGR2GRAY);
+
+		absdiff(imgGray1, imgGray2, imgDiff);
+
 		if(tracking){
 			//tracking action	
 		}
 		
-		imshow("Orginal", imgOrginal);
+		imshow("Orginal", imgDiff);
 
 		int keyCode = waitKey(30);
 		switch(keyCode){
