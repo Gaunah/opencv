00e068a87d3210320fe9f4ccf9075c782653cdf8
gaunah <christoph@zelinski.de>
2015-09-24 13:56:45 +0200

    cannyfilter test

--- cannyfilter.cpp
+++ cannyfilter.cpp
@@ -3,7 +3,54 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
-int main(int argc, char* argv[]){
+int static low = 0, high = 255, sigma = 0;
 
+void createControlPanel(){
+	cv::namedWindow("Control", CV_WINDOW_AUTOSIZE);
+	cvCreateTrackbar("Low", "Control", &low, 255);
+	cvCreateTrackbar("High", "Control", &high, 255);
+	cvCreateTrackbar("Sigma", "Control", &sigma, 5);
+}
+
+int main(int argc, char* argv[]){
+	using namespace cv;
+	VideoCapture cap;
+
+	if(argc == 2){ // open file or cam
+		cap.open(argv[1]);
+	} else {
+		cap.open(0);
+	}
+
+	if(!cap.isOpened()){
+		std::cerr << "could not open file or cam!" << std::endl;
+		return -1;
+	}
+
+	createControlPanel();
+	Mat imgOrginal, imgGray, imgCanny;
+	while(true){ //loop until ESC
+		if(!cap.read(imgOrginal)){
+			std::cerr << "could not read frame from video stream!" << std::endl;
+			return -1;
+		}
+
+		cvtColor(imgOrginal, imgGray, COLOR_BGR2GRAY); //convert to grayscale
+		GaussianBlur(imgGray, imgGray, Size(5, 5), sigma);
+		Canny(imgGray, imgCanny, low, high);
+
+		namedWindow("Orginal", CV_WINDOW_KEEPRATIO);
+		namedWindow("Canny", CV_WINDOW_KEEPRATIO);
+		imshow("Orginal", imgOrginal);
+		imshow("Canny", imgCanny);
+
+		switch(waitKey(10)){
+			case 27: //ESC
+				return 0;
+				break;
+			default:
+				break;	
+		}
+	}
 	return 0;
 }
